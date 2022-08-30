1b310d7c144b618fb5b48173de85d76bbbb4a013
Kelvin Isievwore <kelvinator4leo@gmail.com>
2022-08-30 17:57:17 +0100

    Added Uncles Field To AdvanceBlockchainMessage

--- src/main/java/co/rsk/federate/signing/hsm/message/AdvanceBlockchainMessage.java
+++ src/main/java/co/rsk/federate/signing/hsm/message/AdvanceBlockchainMessage.java
@@ -7,12 +7,17 @@ import org.spongycastle.util.encoders.Hex;
 
 public class AdvanceBlockchainMessage {
     private final List<String> blockHeaders;
+    private final List<String> blockHeadersUncles;
 
-    public AdvanceBlockchainMessage(List<BlockHeader> blockHeaders) {
+    public AdvanceBlockchainMessage(List<BlockHeader> blockHeaders, List<BlockHeader> blockHeadersUncles) {
         this.blockHeaders = new ArrayList<>();
         // Invert order
         for (int index = blockHeaders.size() - 1; index >= 0; index--) {
             this.blockHeaders.add(parseBlockHeader(blockHeaders.get(index)));
+        }
+        this.blockHeadersUncles = new ArrayList<>();
+        for (int index = blockHeadersUncles.size() - 1; index >= 0; index--) {
+            this.blockHeadersUncles.add(parseBlockHeader(blockHeadersUncles.get(index)));
         }
     }
 
@@ -20,7 +25,11 @@ public class AdvanceBlockchainMessage {
         return Hex.toHexString(blockHeader.getFullEncoded());
     }
 
-    public List<String> getData() {
+    public List<String> getBlockHeaders() {
         return blockHeaders;
+    }
+
+    public List<String> getBlockHeadersUncles() {
+        return blockHeadersUncles;
     }
 }
