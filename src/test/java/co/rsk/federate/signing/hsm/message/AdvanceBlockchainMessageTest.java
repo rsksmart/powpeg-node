9dab4c012a87f449d2eb8ac7fd02cdf5bb22f579
Kelvin Isievwore <kelvinator4leo@gmail.com>
2022-08-30 17:57:17 +0100

    Added Uncles Field To AdvanceBlockchainMessage

--- src/test/java/co/rsk/federate/signing/hsm/message/AdvanceBlockchainMessageTest.java
+++ src/test/java/co/rsk/federate/signing/hsm/message/AdvanceBlockchainMessageTest.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.spongycastle.util.encoders.Hex;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -15,20 +16,20 @@ import static org.mockito.Mockito.when;
 public class AdvanceBlockchainMessageTest {
 
     @Test
-    public void getData_ok() {
+    public void getBlockHeaders_ok() {
         byte[] encodedBlockHeader1 = new byte[]{ 1 };
         BlockHeader blockHeader1 = mock(BlockHeader.class);
         when(blockHeader1.getFullEncoded()).thenReturn(encodedBlockHeader1);
 
         List<BlockHeader> blockHeaders = Collections.singletonList(blockHeader1);
-        AdvanceBlockchainMessage message = new AdvanceBlockchainMessage(blockHeaders);
+        AdvanceBlockchainMessage message = new AdvanceBlockchainMessage(blockHeaders, new ArrayList<>());
 
-        Assert.assertEquals(1, message.getData().size());
-        Assert.assertEquals(Hex.toHexString(encodedBlockHeader1), message.getData().get(0));
+        Assert.assertEquals(1, message.getBlockHeaders().size());
+        Assert.assertEquals(Hex.toHexString(encodedBlockHeader1), message.getBlockHeaders().get(0));
     }
 
     @Test
-    public void getData_ok_sort_inverted() {
+    public void getBlockHeaders_ok_sort_inverted() {
         byte[] encodedBlockHeader1 = new byte[]{ 1 };
         BlockHeader blockHeader1 = mock(BlockHeader.class);
         when(blockHeader1.getFullEncoded()).thenReturn(encodedBlockHeader1);
@@ -37,11 +38,11 @@ public class AdvanceBlockchainMessageTest {
         when(blockHeader2.getFullEncoded()).thenReturn(encodedBlockHeader2);
 
         List<BlockHeader> blockHeaders = Arrays.asList(blockHeader1, blockHeader2);
-        AdvanceBlockchainMessage message = new AdvanceBlockchainMessage(blockHeaders);
+        AdvanceBlockchainMessage message = new AdvanceBlockchainMessage(blockHeaders, new ArrayList<>());
 
-        Assert.assertEquals(2, message.getData().size());
-        Assert.assertEquals(Hex.toHexString(encodedBlockHeader2), message.getData().get(0));
-        Assert.assertEquals(Hex.toHexString(encodedBlockHeader1), message.getData().get(1));
+        Assert.assertEquals(2, message.getBlockHeaders().size());
+        Assert.assertEquals(Hex.toHexString(encodedBlockHeader2), message.getBlockHeaders().get(0));
+        Assert.assertEquals(Hex.toHexString(encodedBlockHeader1), message.getBlockHeaders().get(1));
 
     }
 }
