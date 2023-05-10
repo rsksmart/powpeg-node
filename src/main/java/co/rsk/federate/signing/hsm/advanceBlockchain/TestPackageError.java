b8f13b7cd7131fe5723efb3eefd45f8650131539
Kelvin Isievwore <kelvinator4leo@gmail.com>
2023-05-10 18:45:06 +0100

    Test Package Error

---
+++ src/main/java/co/rsk/federate/signing/hsm/advanceBlockchain/TestPackageError.java
@@ -0,0 +1,17 @@
+package co.rsk.federate.signing.hsm.advanceblockchain;
+
+public class TestPackageError {
+    private int test;
+
+    public TestPackageError(int test) {
+        this.test = test;
+    }
+
+    public int getTest() {
+        return test;
+    }
+
+    public void setTest(int test) {
+        this.test = test;
+    }
+}
