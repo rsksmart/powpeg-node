edba196bc401f284a1948b06fe4f82fdc40ae5d4
Kelvin Isievwore <kelvinator4leo@gmail.com>
2023-05-11 13:05:49 +0100

    Renamed HSM2State To PowHSMState

--- src/main/java/co/rsk/federate/signing/hsm/message/PowHSMState.java
+++ src/main/java/co/rsk/federate/signing/hsm/message/PowHSMState.java
@@ -2,12 +2,12 @@ package co.rsk.federate.signing.hsm.message;
 
 import co.rsk.crypto.Keccak256;
 
-public class HSM2State {
+public class PowHSMState {
     private final Keccak256 bestBlockHash;
     private final Keccak256 ancestorBlockHash;
     private final boolean inProgress;
 
-    public HSM2State(String bestBlockHash, String ancestorBlockHash, boolean inProgress) {
+    public PowHSMState(String bestBlockHash, String ancestorBlockHash, boolean inProgress) {
         this.bestBlockHash = new Keccak256(bestBlockHash);
         this.ancestorBlockHash = new Keccak256(ancestorBlockHash);
         this.inProgress = inProgress;
