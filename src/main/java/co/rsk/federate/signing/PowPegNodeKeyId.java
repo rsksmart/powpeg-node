9359127a5bb884d78db79b071cdf574b5f9d51f4
Kelvin Isievwore <kelvinator4leo@gmail.com>
2023-05-24 19:57:33 +0100

    Reordered Variables To Multiple Lines

--- src/main/java/co/rsk/federate/signing/PowPegNodeKeyId.java
+++ src/main/java/co/rsk/federate/signing/PowPegNodeKeyId.java
@@ -4,7 +4,9 @@ package co.rsk.federate.signing;
  * Created by Kelvin Isievwore on 23/05/2023.
  */
 public enum PowPegNodeKeyId {
-    BTC_KEY_ID(new KeyId("BTC")), RSK_KEY_ID(new KeyId("RSK")), MST_KEY_ID(new KeyId("MST"));
+    BTC_KEY_ID(new KeyId("BTC")),
+    RSK_KEY_ID(new KeyId("RSK")),
+    MST_KEY_ID(new KeyId("MST"));
 
     private final KeyId keyId;
 
