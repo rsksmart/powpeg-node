f0bb64fba255fb3c5884f0b4ee2de97275fe86ff
Kelvin Isievwore <kelvinator4leo@gmail.com>
2023-08-03 19:36:29 +0100

    Consider Brothers Difficulty When Accumlating Blocks

--- src/test/java/co/rsk/federate/signing/hsm/advanceblockchain/ConfirmedBlockHeadersProviderTest.java
+++ src/test/java/co/rsk/federate/signing/hsm/advanceblockchain/ConfirmedBlockHeadersProviderTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.mockito.Mockito.mock;
@@ -23,7 +24,7 @@ public class ConfirmedBlockHeadersProviderTest {
     private final BigInteger difficultyCapRegTest = PowHSMBookkeepingConfig.DIFFICULTY_CAP_REGTEST;
 
     @Test
-    public void getConfirmedBlocks_Ok() {
+    public void test_getConfirmedBlocks_Ok() {
 
         Keccak256 startingPoint = TestUtils.createHash(1);
         BlockStore mockBlockStore = mock(BlockStore.class);
@@ -36,9 +37,7 @@ public class ConfirmedBlockHeadersProviderTest {
         for (int i = 11; i < 41; i++) {
             long difficultyValue = 41L - i;
             Block mockBlockToProcess = TestUtils.mockBlock(i, TestUtils.createHash(i), difficultyValue);
-            BlockHeader mockBlockHeaderToProcess = TestUtils.createBlockHeaderMock(i, difficultyValue);
             when(mockBlockStore.getChainBlockByNumber(i)).thenReturn(mockBlockToProcess);
-            when(mockBlockToProcess.getHeader()).thenReturn(mockBlockHeaderToProcess);
             if (i < 37) {
                 expectedBlocks.add(mockBlockToProcess);
             }
@@ -61,7 +60,7 @@ public class ConfirmedBlockHeadersProviderTest {
     }
 
     @Test
-    public void getConfirmedBlocks_MaximumElementsToSend_Ok() {
+    public void test_getConfirmedBlocks_MaximumElementsToSend_Ok() {
 
         Keccak256 startingPoint = TestUtils.createHash(1);
         BlockStore mockBlockStore = mock(BlockStore.class);
@@ -73,9 +72,7 @@ public class ConfirmedBlockHeadersProviderTest {
         for (int i = 11; i < 36; i++) {
             long difficultyValue = 41L - i;
             Block mockBlockToProcess = TestUtils.mockBlock(i, TestUtils.createHash(i), difficultyValue);
-            BlockHeader mockBlockHeaderToProcess = TestUtils.createBlockHeaderMock(i, difficultyValue);
             when(mockBlockStore.getChainBlockByNumber(i)).thenReturn(mockBlockToProcess);
-            when(mockBlockToProcess.getHeader()).thenReturn(mockBlockHeaderToProcess);
         }
 
         ConfirmedBlockHeadersProvider confirmedBlockHeadersProvider = new ConfirmedBlockHeadersProvider(
@@ -92,7 +89,7 @@ public class ConfirmedBlockHeadersProviderTest {
     }
 
     @Test
-    public void getConfirmedBlocks_TooMuchDifficultyExpected_ReturnsEmptyList() {
+    public void test_getConfirmedBlocks_TooMuchDifficultyExpected_ReturnsEmptyList() {
 
         Keccak256 startingPoint = TestUtils.createHash(1);
         BlockStore mockBlockStore = mock(BlockStore.class);
@@ -104,9 +101,7 @@ public class ConfirmedBlockHeadersProviderTest {
         for (int i = 11; i < 21; i++) {
             long difficultyValue = 21L - i;
             Block mockBlockToProcess = TestUtils.mockBlock(i, TestUtils.createHash(i), difficultyValue);
-            BlockHeader mockBlockHeaderToProcess = TestUtils.createBlockHeaderMock(i, difficultyValue);
             when(mockBlockStore.getChainBlockByNumber(i)).thenReturn(mockBlockToProcess);
-            when(mockBlockToProcess.getHeader()).thenReturn(mockBlockHeaderToProcess);
         }
 
         ConfirmedBlockHeadersProvider confirmedBlockHeadersProvider = new ConfirmedBlockHeadersProvider(
@@ -123,7 +118,7 @@ public class ConfirmedBlockHeadersProviderTest {
     }
 
     @Test
-    public void testgetConfirmedBlocksHSMVersion3AboveDifficultyCap() {
+    public void test_getConfirmedBlocksHSMVersion3AboveDifficultyCap() {
 
         Keccak256 startingPoint = TestUtils.createHash(1);
         BlockStore mockBlockStore = mock(BlockStore.class);
@@ -135,9 +130,7 @@ public class ConfirmedBlockHeadersProviderTest {
         for (int i = 11; i < 41; i++) {
             long difficultyValue = 30;
             Block mockBlockToProcess = TestUtils.mockBlock(i, TestUtils.createHash(i), difficultyValue);
-            BlockHeader mockBlockHeaderToProcess = TestUtils.createBlockHeaderMock(i, difficultyValue);
             when(mockBlockStore.getChainBlockByNumber(i)).thenReturn(mockBlockToProcess);
-            when(mockBlockToProcess.getHeader()).thenReturn(mockBlockHeaderToProcess);
         }
 
         ConfirmedBlockHeadersProvider confirmedBlockHeadersProvider = new ConfirmedBlockHeadersProvider(
@@ -154,7 +147,7 @@ public class ConfirmedBlockHeadersProviderTest {
     }
 
     @Test
-    public void testgetConfirmedBlocksHSMVersion3BelowDifficultyCap() {
+    public void test_getConfirmedBlocksHSMVersion3BelowDifficultyCap() {
 
         Keccak256 startingPoint = TestUtils.createHash(1);
         BlockStore mockBlockStore = mock(BlockStore.class);
@@ -166,9 +159,7 @@ public class ConfirmedBlockHeadersProviderTest {
         for (int i = 11; i < 41; i++) {
             long difficultyValue = 15;
             Block mockBlockToProcess = TestUtils.mockBlock(i, TestUtils.createHash(i), difficultyValue);
-            BlockHeader mockBlockHeaderToProcess = TestUtils.createBlockHeaderMock(i, difficultyValue);
             when(mockBlockStore.getChainBlockByNumber(i)).thenReturn(mockBlockToProcess);
-            when(mockBlockToProcess.getHeader()).thenReturn(mockBlockHeaderToProcess);
         }
 
         ConfirmedBlockHeadersProvider confirmedBlockHeadersProvider = new ConfirmedBlockHeadersProvider(
@@ -182,5 +173,38 @@ public class ConfirmedBlockHeadersProviderTest {
 
         // Assert 20 elements in confirmed and 10 in potential list
         Assert.assertEquals(30, confirmedBlocks.size());
+    }
+
+    @Test
+    public void test_getConfirmedBlocks_considerBrothersDifficultyHSMVersion3() {
+
+        Keccak256 startingPoint = TestUtils.createHash(1);
+        BlockStore mockBlockStore = mock(BlockStore.class);
+        Block startingBlock = TestUtils.mockBlock(10, startingPoint);
+        when(mockBlockStore.getBlockByHash(startingPoint.getBytes())).thenReturn(startingBlock);
+        Block mockBestBlock = TestUtils.mockBlock(16, TestUtils.createHash(16));
+        when(mockBlockStore.getBestBlock()).thenReturn(mockBestBlock);
+
+        List<BlockHeader> brothers = Arrays.asList(
+            TestUtils.createBlockHeaderMock(1, 5),
+            TestUtils.createBlockHeaderMock(2, 10));
+
+        for (int i = 11; i < 17; i++) {
+            long difficultyValue = 15;
+            Block mockBlockToProcess = TestUtils.mockBlockWithBrothers(i, TestUtils.createHash(i), difficultyValue, brothers);
+            when(mockBlockStore.getChainBlockByNumber(i)).thenReturn(mockBlockToProcess);
+        }
+
+        ConfirmedBlockHeadersProvider confirmedBlockHeadersProvider = new ConfirmedBlockHeadersProvider(
+            new BigInteger("160"),
+            100,
+            mockBlockStore,
+            BigInteger.valueOf(50),
+            HSM_VERSION_3);
+
+        List<Block> confirmedBlocks = confirmedBlockHeadersProvider.getConfirmedBlocks(startingPoint);
+
+        // Assert 1 element in confirmed and 5 in potential list
+        Assert.assertEquals(6, confirmedBlocks.size());
     }
 }
