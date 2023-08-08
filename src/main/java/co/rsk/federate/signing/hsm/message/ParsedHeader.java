d5cc15231479f773304c2ff2d4d05e165a12b37e
Kelvin Isievwore <kelvinator4leo@gmail.com>
2023-08-08 17:14:41 +0100

    Added New ParsedHeader Class

---
+++ src/main/java/co/rsk/federate/signing/hsm/message/ParsedHeader.java
@@ -0,0 +1,35 @@
+package co.rsk.federate.signing.hsm.message;
+
+import org.ethereum.core.BlockHeader;
+import org.spongycastle.util.encoders.Hex;
+
+import java.util.List;
+
+/**
+ * Created by Kelvin Isievwore on 01/08/2023.
+ */
+public class ParsedHeader {
+    private final String blockHeader;
+    private final String[] brothers;
+
+    public ParsedHeader(BlockHeader blockHeader, List<BlockHeader> brothers) {
+        this.blockHeader = serializeBlockHeader(blockHeader);
+        this.brothers = serializeBrothers(brothers);
+    }
+
+    public String getBlockHeader() {
+        return blockHeader;
+    }
+
+    public String[] getBrothers() {
+        return brothers.clone();
+    }
+
+    private String serializeBlockHeader(BlockHeader blockHeader) {
+        return Hex.toHexString(blockHeader.getFullEncoded());
+    }
+
+    private String[] serializeBrothers(List<BlockHeader> brothers) {
+        return brothers.stream().map(this::serializeBlockHeader).toArray(String[]::new);
+    }
+}
