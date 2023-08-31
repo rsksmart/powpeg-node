5fd7f34e368f18c6a7d23a7abf34d6365c6f6524
Marcos <marcos@iovlabs.org>
2023-08-31 16:29:12 -0300

    Update FederatorTest to use junit 5

--- src/test/java/co/rsk/federate/FederatorTest.java
+++ src/test/java/co/rsk/federate/FederatorTest.java
@@ -1,81 +1,85 @@
 package co.rsk.federate;
 
-import co.rsk.federate.signing.ECDSASigner;
-import co.rsk.federate.signing.KeyId;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.io.IOException;
-import java.net.UnknownHostException;
-import java.security.Key;
-import java.util.*;
-
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import co.rsk.federate.signing.ECDSASigner;
+import co.rsk.federate.signing.KeyId;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 /**
  * Created by ajlopez on 12/01/2017.
  */
-public class FederatorTest {
+class FederatorTest {
 
     private FederatorPeersChecker peersChecker;
     private ECDSASigner signer;
     private Federator federator;
-    private List<KeyId> requiredKeys;
 
-    @Before
-    public void init() throws IOException {
+    @BeforeEach
+    void init() {
         signer = mock(ECDSASigner.class);
         peersChecker = mock(FederatorPeersChecker.class);
-        requiredKeys = Arrays.asList(new KeyId("a-key-id"), new KeyId("another-key-id"));
+
+        List<KeyId> requiredKeys = Arrays.asList(
+            new KeyId("a-key-id"),
+            new KeyId("another-key-id")
+        );
         federator = new Federator(signer, requiredKeys, peersChecker);
     }
 
     @Test
-    public void validWhenCheckerAndSignerValid() throws UnknownHostException {
+    void validWhenCheckerAndSignerValid() throws UnknownHostException {
         when(peersChecker.checkPeerAddresses()).thenReturn(Collections.emptyList());
         when(signer.check()).thenReturn(getCheckResult(true));
         when(signer.canSignWith(any(KeyId.class))).thenReturn(true);
 
-        Assert.assertTrue(federator.validFederator());
+        assertTrue(federator.validFederator());
     }
 
     @Test
-    public void invalidWhenCheckerInvalid() throws UnknownHostException {
+    void invalidWhenCheckerInvalid() throws UnknownHostException {
         when(peersChecker.checkPeerAddresses()).thenReturn(Arrays.asList("error A", "error B"));
         when(signer.check()).thenReturn(getCheckResult(true));
         when(signer.canSignWith(any(KeyId.class))).thenReturn(true);
 
-        Assert.assertFalse(federator.validFederator());
+        assertFalse(federator.validFederator());
     }
 
     @Test
-    public void invalidWhenSignerInvalid() throws UnknownHostException {
+    void invalidWhenSignerInvalid() throws UnknownHostException {
         when(peersChecker.checkPeerAddresses()).thenReturn(Collections.emptyList());
         when(signer.check()).thenReturn(getCheckResult(false));
         when(signer.canSignWith(any(KeyId.class))).thenReturn(true);
 
-        Assert.assertFalse(federator.validFederator());
+        assertFalse(federator.validFederator());
     }
 
     @Test
-    public void invalidWhenSignerIsMissingAKey() throws UnknownHostException {
+    void invalidWhenSignerIsMissingAKey() throws UnknownHostException {
         when(peersChecker.checkPeerAddresses()).thenReturn(Collections.emptyList());
         when(signer.check()).thenReturn(getCheckResult(true));
         when(signer.canSignWith(new KeyId("a-key-id"))).thenReturn(true);
         when(signer.canSignWith(new KeyId("another-key-id"))).thenReturn(false);
 
-        Assert.assertFalse(federator.validFederator());
+        assertFalse(federator.validFederator());
     }
 
     @Test
-    public void invalidWhenCheckerAndSignerInalid() throws UnknownHostException {
+    void invalidWhenCheckerAndSignerInalid() throws UnknownHostException {
         when(peersChecker.checkPeerAddresses()).thenReturn(Arrays.asList("error A", "error B"));
         when(signer.check()).thenReturn(getCheckResult(false));
 
-        Assert.assertFalse(federator.validFederator());
+        assertFalse(federator.validFederator());
     }
 
     private ECDSASigner.ECDSASignerCheckResult getCheckResult(boolean success) {
