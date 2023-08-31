fff29b1131f35d390814f4bdd35763ce60a52bc9
Marcos <marcos@iovlabs.org>
2023-08-31 16:17:33 -0300

    Update CoinbaseInformationTest to use junit 5

--- src/test/java/co/rsk/federate/CoinbaseInformationTest.java
+++ src/test/java/co/rsk/federate/CoinbaseInformationTest.java
@@ -1,61 +1,72 @@
 package co.rsk.federate;
 
-import org.bitcoinj.core.*;
-import org.bitcoinj.params.RegTestParams;
-import org.junit.Assert;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import org.bitcoinj.core.Address;
+import org.bitcoinj.core.Coin;
+import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.core.PartialMerkleTree;
+import org.bitcoinj.core.Sha256Hash;
+import org.bitcoinj.core.Transaction;
+import org.bitcoinj.core.TransactionInput;
+import org.bitcoinj.core.TransactionOutput;
+import org.bitcoinj.core.TransactionWitness;
+import org.bitcoinj.params.RegTestParams;
+import org.junit.jupiter.api.Test;
 
-public class CoinbaseInformationTest {
+class CoinbaseInformationTest {
 
-    private NetworkParameters parameters = RegTestParams.get();
+    private final NetworkParameters parameters = RegTestParams.get();
 
-    @Test(expected = Exception.class)
-    public void cant_instanciate_if_no_data() throws Exception {
-        new CoinbaseInformation(null, null, null, null);
-    }
-
-    @Test(expected = Exception.class)
-    public void cant_instanciate_if_no_coinbase_transaction() throws Exception {
-        new CoinbaseInformation(getTx(false, false), null, null, null);
-    }
-
-    @Test(expected = Exception.class)
-    public void cant_instanciate_if_coinbase_transaction_without_witness() throws Exception {
-        new CoinbaseInformation(getTx(true, false), null, null, null);
+    @Test
+    void cant_instanciate_if_no_data() {
+        assertThrows(Exception.class, () -> new CoinbaseInformation(null, null, null, null));
     }
 
     @Test
-    public void instanciates_coinbase_transaction() throws Exception {
+    void cant_instanciate_if_no_coinbase_transaction() {
+        assertThrows(Exception.class, () -> new CoinbaseInformation(getTx(false, false), null, null, null));
+    }
+
+    @Test
+    void cant_instanciate_if_coinbase_transaction_without_witness() {
+        assertThrows(Exception.class, () -> new CoinbaseInformation(getTx(true, false), null, null, null));
+    }
+
+    @Test
+    void instanciates_coinbase_transaction() throws Exception {
         Transaction tx = getTx();
         CoinbaseInformation ci = new CoinbaseInformation(tx, null, null, null);
-        Assert.assertEquals(tx, ci.getCoinbaseTransaction());
+        assertEquals(tx, ci.getCoinbaseTransaction());
     }
 
     @Test
-    public void sets_ready_to_inform() throws Exception {
+    void sets_ready_to_inform() throws Exception {
         CoinbaseInformation coinbaseInformation = new CoinbaseInformation(getTx(), null, null, null);
-        Assert.assertFalse(coinbaseInformation.isReadyToInform());
+        assertFalse(coinbaseInformation.isReadyToInform());
         coinbaseInformation.setReadyToInform(true);
-        Assert.assertTrue(coinbaseInformation.isReadyToInform());
+        assertTrue(coinbaseInformation.isReadyToInform());
     }
 
     @Test
-    public void gets_witness_reserved_value() throws Exception {
+    void gets_witness_reserved_value() throws Exception {
         Transaction tx = getTx();
         tx.getInput(0).getWitness().setPush(0, Sha256Hash.ZERO_HASH.getBytes());
         CoinbaseInformation coinbaseInformation = new CoinbaseInformation(tx, null, null, null);
-        Assert.assertEquals(Sha256Hash.ZERO_HASH.getBytes(), coinbaseInformation.getCoinbaseWitnessReservedValue());
+        assertEquals(Sha256Hash.ZERO_HASH.getBytes(), coinbaseInformation.getCoinbaseWitnessReservedValue());
     }
 
     @Test
-    public void serialize_and_deserialize() throws Exception {
-        List<Sha256Hash> hashes =  Arrays.asList(Sha256Hash.ZERO_HASH);
+    void serialize_and_deserialize() throws Exception {
+        List<Sha256Hash> hashes = Collections.singletonList(Sha256Hash.ZERO_HASH);
         PartialMerkleTree pmt = new PartialMerkleTree(parameters, new byte[] {}, hashes, hashes.size());
         CoinbaseInformation coinbaseInformation = new CoinbaseInformation(getTx(), Sha256Hash.ZERO_HASH, Sha256Hash.ZERO_HASH, pmt);
-        Assert.assertEquals(coinbaseInformation,CoinbaseInformation.fromRlp(coinbaseInformation.serializeToRLP(), parameters));
+        assertEquals(coinbaseInformation,CoinbaseInformation.fromRlp(coinbaseInformation.serializeToRLP(), parameters));
     }
 
     private Transaction getTx() {
