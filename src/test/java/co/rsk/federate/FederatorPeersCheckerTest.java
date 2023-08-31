3c0c4d4e89f46a8adc1b02250f2cebbbe662fd56
Marcos <marcos@iovlabs.org>
2023-08-31 16:25:15 -0300

    Update FederatorPeersCheckerTest to use junit 5

--- src/test/java/co/rsk/federate/FederatorPeersCheckerTest.java
+++ src/test/java/co/rsk/federate/FederatorPeersCheckerTest.java
@@ -1,37 +1,37 @@
 package co.rsk.federate;
 
-import org.bitcoinj.params.RegTestParams;
-import org.junit.Assert;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
+import org.bitcoinj.params.RegTestParams;
+import org.junit.jupiter.api.Test;
 
 /**
  * Created by ajlopez on 30/12/2016.
  */
-public class FederatorPeersCheckerTest {
+class FederatorPeersCheckerTest {
 
     public static final int DEFAULT_PORT = 18332;
 
     @Test
-    public void invalidListIfNull() throws UnknownHostException {
+    void invalidListIfNull() throws UnknownHostException {
         FederatorPeersChecker checker = new FederatorPeersChecker(DEFAULT_PORT, null, RegTestParams.get());
-        Assert.assertEquals("No Bitcoin Peers", checker.checkPeerAddresses().get(0));
+        assertEquals("No Bitcoin Peers", checker.checkPeerAddresses().get(0));
     }
 
     @Test
-    public void invalidListIfEmpty() throws UnknownHostException {
+    void invalidListIfEmpty() throws UnknownHostException {
         FederatorPeersChecker checker = new FederatorPeersChecker(DEFAULT_PORT, new ArrayList<>(), RegTestParams.get());
-        Assert.assertEquals("No Bitcoin Peers", checker.checkPeerAddresses().get(0));
+        assertEquals("No Bitcoin Peers", checker.checkPeerAddresses().get(0));
     }
 
     @Test
-    public void onePeer() throws UnknownHostException {
+    void onePeer() throws UnknownHostException {
         List<String> peers = new ArrayList<>();
         peers.add("localhost:16000");
         FederatorPeersChecker checker = new FederatorPeersChecker(16000, peers, RegTestParams.get());
-        Assert.assertEquals("Cannot connect to Bitcoin node localhost:16000", checker.checkPeerAddresses().get(0));
+        assertEquals("Cannot connect to Bitcoin node localhost:16000", checker.checkPeerAddresses().get(0));
     }
 }
