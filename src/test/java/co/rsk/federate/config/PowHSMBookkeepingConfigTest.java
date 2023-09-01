9e3e362cb7f7ed43d53fbcd3c283fb21fb787c06
Marcos <marcos@iovlabs.org>
2023-08-31 22:11:08 -0300

    Update PowHSMBookkeepingConfigTest to use junit 5

--- src/test/java/co/rsk/federate/config/PowHSMBookkeepingConfigTest.java
+++ src/test/java/co/rsk/federate/config/PowHSMBookkeepingConfigTest.java
@@ -1,13 +1,14 @@
 package co.rsk.federate.config;
 
-import co.rsk.bitcoinj.core.NetworkParameters;
-import com.typesafe.config.Config;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import co.rsk.bitcoinj.core.NetworkParameters;
+import com.typesafe.config.Config;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /**
  * Test class for PowHSMBookkeepingConfig.
@@ -15,42 +16,42 @@ import static org.mockito.Mockito.when;
  * @author kelvin.isievwore
  */
 
-public class PowHSMBookkeepingConfigTest {
+class PowHSMBookkeepingConfigTest {
 
     private PowHSMBookkeepingConfig powHsmBookkeepingConfig;
     private SignerConfig signerConfigMock;
     private Config configMock;
 
-    @Before
-    public void setup() {
+    @BeforeEach
+    void setup() {
         signerConfigMock = mock(SignerConfig.class);
         configMock = mock(Config.class);
     }
 
     @Test
-    public void testGetDifficultyCapForMainnet() {
+    void testGetDifficultyCapForMainnet() {
         when(signerConfigMock.getConfig()).thenReturn(configMock);
         powHsmBookkeepingConfig = new PowHSMBookkeepingConfig(signerConfigMock, NetworkParameters.ID_MAINNET);
-        Assert.assertEquals(PowHSMBookkeepingConfig.DIFFICULTY_CAP_MAINNET, powHsmBookkeepingConfig.getDifficultyCap());
+        assertEquals(PowHSMBookkeepingConfig.DIFFICULTY_CAP_MAINNET, powHsmBookkeepingConfig.getDifficultyCap());
     }
 
     @Test
-    public void testGetDifficultyCapForTestnet() {
+    void testGetDifficultyCapForTestnet() {
         when(signerConfigMock.getConfig()).thenReturn(configMock);
         powHsmBookkeepingConfig = new PowHSMBookkeepingConfig(signerConfigMock, NetworkParameters.ID_TESTNET);
-        Assert.assertEquals(PowHSMBookkeepingConfig.DIFFICULTY_CAP_TESTNET, powHsmBookkeepingConfig.getDifficultyCap());
+        assertEquals(PowHSMBookkeepingConfig.DIFFICULTY_CAP_TESTNET, powHsmBookkeepingConfig.getDifficultyCap());
     }
 
     @Test
-    public void testGetDifficultyCapForRegtest() {
+    void testGetDifficultyCapForRegtest() {
         when(signerConfigMock.getConfig()).thenReturn(configMock);
         powHsmBookkeepingConfig = new PowHSMBookkeepingConfig(signerConfigMock, NetworkParameters.ID_REGTEST);
-        Assert.assertEquals(PowHSMBookkeepingConfig.DIFFICULTY_CAP_REGTEST, powHsmBookkeepingConfig.getDifficultyCap());
+        assertEquals(PowHSMBookkeepingConfig.DIFFICULTY_CAP_REGTEST, powHsmBookkeepingConfig.getDifficultyCap());
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testGetDifficultyCapForInvalidNetwork() {
+    @Test
+    void testGetDifficultyCapForInvalidNetwork() {
         when(signerConfigMock.getConfig()).thenReturn(configMock);
-        new PowHSMBookkeepingConfig(signerConfigMock, NetworkParameters.ID_UNITTESTNET);
+        assertThrows(IllegalArgumentException.class, () -> new PowHSMBookkeepingConfig(signerConfigMock, NetworkParameters.ID_UNITTESTNET));
     }
 }
