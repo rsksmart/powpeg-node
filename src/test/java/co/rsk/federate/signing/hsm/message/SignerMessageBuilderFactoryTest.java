fccdc63c3080f2e19c2e793b47d703a2b33cc3fa
Marcos <marcos@iovlabs.org>
2023-09-14 18:50:49 -0300

    Update SignerMessageBuilderFactoryTest to use junit 5

--- src/test/java/co/rsk/federate/signing/hsm/message/SignerMessageBuilderFactoryTest.java
+++ src/test/java/co/rsk/federate/signing/hsm/message/SignerMessageBuilderFactoryTest.java
@@ -1,5 +1,10 @@
 package co.rsk.federate.signing.hsm.message;
 
+import static co.rsk.federate.signing.utils.TestUtils.createHash;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+
 import co.rsk.bitcoinj.core.BtcTransaction;
 import co.rsk.crypto.Keccak256;
 import co.rsk.federate.signing.hsm.HSMClientException;
@@ -7,49 +12,52 @@ import co.rsk.federate.signing.hsm.HSMUnsupportedVersionException;
 import co.rsk.federate.signing.utils.TestUtils;
 import org.ethereum.core.TransactionReceipt;
 import org.ethereum.db.ReceiptStore;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static co.rsk.federate.signing.utils.TestUtils.createHash;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-
-public class SignerMessageBuilderFactoryTest {
+class SignerMessageBuilderFactoryTest {
 
     private SignerMessageBuilderFactory factory;
 
-    @Before
+    @BeforeEach
     public void createFactory() {
         factory = new SignerMessageBuilderFactory(mock(ReceiptStore.class));
     }
 
-    @Test(expected = HSMUnsupportedVersionException.class)
-    public void buildWithWrongVersion() throws HSMClientException {
-        factory.buildFromConfig(-5, mock(ReleaseCreationInformation.class));
+    @Test
+    void buildWithWrongVersion() {
+        assertThrows(HSMUnsupportedVersionException.class, () -> factory.buildFromConfig(
+            -5,
+            mock(ReleaseCreationInformation.class)
+        ));
+
     }
 
     @Test
-    public void buildFromHSMVersion1() throws HSMClientException {
-        SignerMessageBuilder sigMessVersion1 = factory.buildFromConfig(1, mock(ReleaseCreationInformation.class));
+    void buildFromHSMVersion1() throws HSMClientException {
+        SignerMessageBuilder sigMessVersion1 = factory.buildFromConfig(
+            1,
+            mock(ReleaseCreationInformation.class)
+        );
         assertTrue(sigMessVersion1 instanceof SignerMessageBuilderV1);
     }
 
     @Test
-    public void buildFromConfig_hsm_2_ok() throws HSMClientException {
+    void buildFromConfig_hsm_2_ok() throws HSMClientException {
         test_buildFromConfig_hsm(2);
     }
 
     @Test
-    public void buildFromConfig_hsm_3_ok() throws HSMClientException {
+    void buildFromConfig_hsm_3_ok() throws HSMClientException {
         test_buildFromConfig_hsm(3);
     }
 
     @Test
-    public void buildFromConfig_hsm_4_ok() throws HSMClientException {
+    void buildFromConfig_hsm_4_ok() throws HSMClientException {
         test_buildFromConfig_hsm(4);
     }
 
-    public void test_buildFromConfig_hsm(int version) throws HSMUnsupportedVersionException {
+    void test_buildFromConfig_hsm(int version) throws HSMUnsupportedVersionException {
         SignerMessageBuilder messageBuilder = factory.buildFromConfig(
             version,
             new ReleaseCreationInformation(
