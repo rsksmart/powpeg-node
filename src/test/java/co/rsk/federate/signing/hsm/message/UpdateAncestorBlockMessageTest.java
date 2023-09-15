44737ca8f511243ae3f49b198d667372b07a5454
Marcos <marcos@iovlabs.org>
2023-09-15 13:04:21 -0300

    Update UpdateAncestorBlockMessage to use junit 5

--- src/test/java/co/rsk/federate/signing/hsm/message/UpdateAncestorBlockMessageTest.java
+++ src/test/java/co/rsk/federate/signing/hsm/message/UpdateAncestorBlockMessageTest.java
@@ -1,47 +1,45 @@
 package co.rsk.federate.signing.hsm.message;
 
-import org.ethereum.core.BlockHeader;
-import org.junit.Assert;
-import org.junit.Test;
-import org.spongycastle.util.encoders.Hex;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import org.ethereum.config.blockchain.upgrades.ActivationConfig;
+import org.ethereum.core.BlockHeader;
+import org.ethereum.core.BlockHeaderBuilder;
+import org.junit.jupiter.api.Test;
+import org.spongycastle.util.encoders.Hex;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+class UpdateAncestorBlockMessageTest {
 
-public class UpdateAncestorBlockMessageTest {
+    private BlockHeaderBuilder blockHeaderBuilder = new BlockHeaderBuilder(mock(ActivationConfig.class));
 
-//    @Test
-//    public void getData_ok() {
-//        byte[] encodedBlockHeader1 = new byte[]{ 1 };
-//        BlockHeader blockHeader1 = mock(BlockHeader.class);
-//        when(blockHeader1.getEncoded(false, false )).thenReturn(encodedBlockHeader1);
-//
-//        List<BlockHeader> blockHeaders = Collections.singletonList(blockHeader1);
-//        UpdateAncestorBlockMessage message = new UpdateAncestorBlockMessage(blockHeaders);
-//
-//        Assert.assertEquals(1, message.getData().size());
-//        Assert.assertEquals(Hex.toHexString(encodedBlockHeader1), message.getData().get(0));
-//    }
+    @Test
+    void getData_ok() {
+        BlockHeader blockHeader1 = blockHeaderBuilder.setNumber(1).build();
 
-//    @Test
-//    public void getData_ok_sort_inverted() {
-//        byte[] encodedBlockHeader1 = new byte[]{ 1 };
-//        BlockHeader blockHeader1 = mock(BlockHeader.class);
-//        when(blockHeader1.getEncoded(false, false)).thenReturn(encodedBlockHeader1);
-//        byte[] encodedBlockHeader2 = new byte[]{ 2 };
-//        BlockHeader blockHeader2 = mock(BlockHeader.class);
-//        when(blockHeader2.getEncoded(false, false)).thenReturn(encodedBlockHeader2);
-//
-//        List<BlockHeader> blockHeaders = Arrays.asList(blockHeader1, blockHeader2);
-//        UpdateAncestorBlockMessage message = new UpdateAncestorBlockMessage(blockHeaders);
-//
-//        Assert.assertEquals(2, message.getData().size());
-//        Assert.assertEquals(Hex.toHexString(encodedBlockHeader2), message.getData().get(0));
-//        Assert.assertEquals(Hex.toHexString(encodedBlockHeader1), message.getData().get(1));
-//
-//    }
+        List<BlockHeader> blockHeaders = Collections.singletonList(blockHeader1);
+        UpdateAncestorBlockMessage message = new UpdateAncestorBlockMessage(blockHeaders);
+
+        assertEquals(1, message.getData().size());
+        assertEquals(
+            Hex.toHexString(blockHeader1.getEncoded(false, false)),
+            message.getData().get(0)
+        );
+    }
+
+    @Test
+    void getData_ok_sort_inverted() {
+        BlockHeader blockHeader1 = blockHeaderBuilder.setNumber(1).build();
+        BlockHeader blockHeader2 = blockHeaderBuilder.setNumber(2).build();
+
+        List<BlockHeader> blockHeaders = Arrays.asList(blockHeader1, blockHeader2);
+        UpdateAncestorBlockMessage message = new UpdateAncestorBlockMessage(blockHeaders);
+
+        assertEquals(2, message.getData().size());
+        assertEquals(Hex.toHexString(blockHeader1.getEncoded(false, false)), message.getData().get(0));
+        assertEquals(Hex.toHexString(blockHeader2.getEncoded(false, false)), message.getData().get(1));
+    }
 }
