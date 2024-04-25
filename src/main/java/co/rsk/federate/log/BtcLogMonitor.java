5fe228b6d079a46332f519657ff5e4a94e69d510
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2024-04-25 15:11:31 +0000

    feat: add java formatter using google coding style

--- src/main/java/co/rsk/federate/log/BtcLogMonitor.java
+++ src/main/java/co/rsk/federate/log/BtcLogMonitor.java
@@ -4,15 +4,15 @@ import co.rsk.federate.bitcoin.BitcoinWrapper;
 
 public class BtcLogMonitor {
 
-    private final BitcoinWrapper bitcoinWrapper;
-    private final FederateLogger monitoringLogger;
+  private final BitcoinWrapper bitcoinWrapper;
+  private final FederateLogger monitoringLogger;
 
-    public BtcLogMonitor(BitcoinWrapper bitcoinWrapper, FederateLogger monitoringLogger) {
-        this.bitcoinWrapper = bitcoinWrapper;
-        this.monitoringLogger = monitoringLogger;
-    }
+  public BtcLogMonitor(BitcoinWrapper bitcoinWrapper, FederateLogger monitoringLogger) {
+    this.bitcoinWrapper = bitcoinWrapper;
+    this.monitoringLogger = monitoringLogger;
+  }
 
-    public void start() {
-        bitcoinWrapper.addNewBlockListener(monitoringLogger::setCurrentBtcBestBlock);
-    }
+  public void start() {
+    bitcoinWrapper.addNewBlockListener(monitoringLogger::setCurrentBtcBestBlock);
+  }
 }
--- src/test/java/co/rsk/federate/log/FederateLoggerTest.java
+++ src/test/java/co/rsk/federate/log/FederateLoggerTest.java
@@ -20,83 +20,68 @@ import org.slf4j.Logger;
 
 class FederateLoggerTest {
 
-    @Test
-    void logOk() {
-        FederatorSupport federatorSupport = mock(FederatorSupport.class);
-        AtomicLong currentTime = new AtomicLong(1);
-        Logger logger = mock(Logger.class);
-        LoggerProvider loggerProvider = () -> logger;
-        FederateLogger monitoringLogger = new FederateLogger(
-                federatorSupport,
-            currentTime::get,
-            loggerProvider,
-            0,
-            0
-        );
+  @Test
+  void logOk() {
+    FederatorSupport federatorSupport = mock(FederatorSupport.class);
+    AtomicLong currentTime = new AtomicLong(1);
+    Logger logger = mock(Logger.class);
+    LoggerProvider loggerProvider = () -> logger;
+    FederateLogger monitoringLogger =
+        new FederateLogger(federatorSupport, currentTime::get, loggerProvider, 0, 0);
 
-        int bridgeBtcHeight = 1;
-        doReturn(bridgeBtcHeight).when(federatorSupport).getBtcBlockchainBestChainHeight();
+    int bridgeBtcHeight = 1;
+    doReturn(bridgeBtcHeight).when(federatorSupport).getBtcBlockchainBestChainHeight();
 
-        long rskBestBlockHeight = 1L;
-        Block currentRskBestBlock = mock(Block.class);
-        doReturn(rskBestBlockHeight).when(currentRskBestBlock).getNumber();
-        monitoringLogger.setCurrentRskBestBlock(currentRskBestBlock);
+    long rskBestBlockHeight = 1L;
+    Block currentRskBestBlock = mock(Block.class);
+    doReturn(rskBestBlockHeight).when(currentRskBestBlock).getNumber();
+    monitoringLogger.setCurrentRskBestBlock(currentRskBestBlock);
 
-        int btcBestBlockHeight = 1;
-        StoredBlock currentBtcBestBlock = mock(StoredBlock.class);
-        doReturn(btcBestBlockHeight).when(currentBtcBestBlock).getHeight();
-        monitoringLogger.setCurrentBtcBestBlock(currentBtcBestBlock);
+    int btcBestBlockHeight = 1;
+    StoredBlock currentBtcBestBlock = mock(StoredBlock.class);
+    doReturn(btcBestBlockHeight).when(currentBtcBestBlock).getHeight();
+    monitoringLogger.setCurrentBtcBestBlock(currentBtcBestBlock);
 
-        monitoringLogger.log();
+    monitoringLogger.log();
 
-        ArgumentCaptor<Object> loggerArgumentsCaptor = ArgumentCaptor.forClass(Object.class);
-        verify(logger, times(3)).info(anyString(), loggerArgumentsCaptor.capture());
-        List<Object> loggerArguments = loggerArgumentsCaptor.getAllValues();
-        assertEquals(rskBestBlockHeight, loggerArguments.get(0));
-        assertEquals((long) btcBestBlockHeight, loggerArguments.get(1));
-        assertEquals(bridgeBtcHeight, loggerArguments.get(2));
-    }
+    ArgumentCaptor<Object> loggerArgumentsCaptor = ArgumentCaptor.forClass(Object.class);
+    verify(logger, times(3)).info(anyString(), loggerArgumentsCaptor.capture());
+    List<Object> loggerArguments = loggerArgumentsCaptor.getAllValues();
+    assertEquals(rskBestBlockHeight, loggerArguments.get(0));
+    assertEquals((long) btcBestBlockHeight, loggerArguments.get(1));
+    assertEquals(bridgeBtcHeight, loggerArguments.get(2));
+  }
 
-    @Test
-    void dontLogOnEveryBlock() {
-        FederatorSupport federatorSupport = mock(FederatorSupport.class);
-        AtomicLong currentTime = new AtomicLong(1);
-        Logger logger = mock(Logger.class);
-        LoggerProvider loggerProvider = () -> logger;
-        FederateLogger monitoringLogger = new FederateLogger(
-            federatorSupport,
-            currentTime::get,
-            loggerProvider,
-            0,
-            1
-        );
+  @Test
+  void dontLogOnEveryBlock() {
+    FederatorSupport federatorSupport = mock(FederatorSupport.class);
+    AtomicLong currentTime = new AtomicLong(1);
+    Logger logger = mock(Logger.class);
+    LoggerProvider loggerProvider = () -> logger;
+    FederateLogger monitoringLogger =
+        new FederateLogger(federatorSupport, currentTime::get, loggerProvider, 0, 1);
 
-        long rskBestBlockHeight = 1L;
-        Block currentRskBestBlock = mock(Block.class);
-        doReturn(rskBestBlockHeight).when(currentRskBestBlock).getNumber();
-        monitoringLogger.setCurrentRskBestBlock(currentRskBestBlock);
+    long rskBestBlockHeight = 1L;
+    Block currentRskBestBlock = mock(Block.class);
+    doReturn(rskBestBlockHeight).when(currentRskBestBlock).getNumber();
+    monitoringLogger.setCurrentRskBestBlock(currentRskBestBlock);
 
-        monitoringLogger.log();
+    monitoringLogger.log();
 
-        verifyNoInteractions(logger);
-    }
+    verifyNoInteractions(logger);
+  }
 
-    @Test
-    void dontFloodLogger() {
-        FederatorSupport federatorSupport = mock(FederatorSupport.class);
-        AtomicLong currentTime = new AtomicLong(1);
-        Logger logger = mock(Logger.class);
-        LoggerProvider loggerProvider = () -> logger;
-        FederateLogger monitoringLogger = new FederateLogger(
-            federatorSupport,
-            currentTime::get,
-            loggerProvider,
-            1,
-            0
-        );
+  @Test
+  void dontFloodLogger() {
+    FederatorSupport federatorSupport = mock(FederatorSupport.class);
+    AtomicLong currentTime = new AtomicLong(1);
+    Logger logger = mock(Logger.class);
+    LoggerProvider loggerProvider = () -> logger;
+    FederateLogger monitoringLogger =
+        new FederateLogger(federatorSupport, currentTime::get, loggerProvider, 1, 0);
 
-        monitoringLogger.log();
+    monitoringLogger.log();
 
-        verifyNoInteractions(logger);
-    }
+    verifyNoInteractions(logger);
+  }
 }
