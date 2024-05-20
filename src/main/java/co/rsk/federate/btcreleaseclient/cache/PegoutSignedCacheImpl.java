df4d3838d8589c8b0b69e20e5fd7983a1c228d02
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2024-05-20 14:45:09 +0000

    feat(pegout): add periodic cleaner for pegout signed cache
    refactor(pegout): minor refactor for pegout signed cache periodic cleaner
    
    feat(pegout): add logs for pegout signed cache periodic cleaner
    
    refactor(pegout): change to use minus method from Duration class for all unit tests related to pegouts signed cache
    
    refactor(pegout): change expired to valid references in pegouts signed cache

--- src/main/java/co/rsk/federate/btcreleaseclient/cache/PegoutSignedCacheImpl.java
+++ src/main/java/co/rsk/federate/btcreleaseclient/cache/PegoutSignedCacheImpl.java
@@ -6,27 +6,39 @@ import java.time.Instant;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-class PegoutSignedCacheImpl implements PegoutSignedCache {
+public class PegoutSignedCacheImpl implements PegoutSignedCache {
 
   private static final Logger logger = LoggerFactory.getLogger(PegoutSignedCacheImpl.class);
+  private static final Integer CLEANUP_INTERVAL_IN_HOURS = 1;
 
   private final Map<Keccak256, Instant> cache = new ConcurrentHashMap<>();
+  private final ScheduledExecutorService cleanupScheduler = Executors.newSingleThreadScheduledExecutor();
   private final Duration ttl;
 
-  PegoutSignedCacheImpl(Duration ttl) {
-    assertValidTtl(ttl);
-
+  public PegoutSignedCacheImpl(Duration ttl) {
+    validateTtl(ttl);
     this.ttl = ttl;
+
+    // Start a background thread for periodic cleanup
+    cleanupScheduler.scheduleAtFixedRate(
+        this::performCleanup,
+        CLEANUP_INTERVAL_IN_HOURS, // initial delay
+        CLEANUP_INTERVAL_IN_HOURS, // period
+        TimeUnit.HOURS
+    );
   }
 
   @Override
   public boolean hasAlreadyBeenSigned(Keccak256 pegoutCreationRskTxHash) {
     return Optional.ofNullable(pegoutCreationRskTxHash)
         .map(cache::get)
-        .map(this::hasTimestampNotExpired)
+        .map(this::isValidTimestamp)
         .orElse(false);
   }
 
@@ -41,19 +53,28 @@ class PegoutSignedCacheImpl implements PegoutSignedCache {
         .ifPresent(rskTxHash -> cache.putIfAbsent(rskTxHash, Instant.now()));
   }
 
-  private boolean hasTimestampNotExpired(Instant timestampInCache) {
+  void performCleanup() {
+    logger.trace(
+        "[performCleanup] Pegouts signed cache before cleanup: {}", cache.keySet());
+    cache.entrySet().removeIf(
+        entry -> !isValidTimestamp(entry.getValue()));
+    logger.trace(
+        "[performCleanup] Pegouts signed cache after cleanup: {}", cache.keySet());
+  }
+
+  private boolean isValidTimestamp(Instant timestampInCache) {
     return Optional.ofNullable(timestampInCache)
         .map(timestamp -> Instant.now().toEpochMilli() - timestamp.toEpochMilli())
         .map(timeCachedInMillis -> timeCachedInMillis <= ttl.toMillis())
         .orElse(false);
   }
 
-  private static void assertValidTtl(Duration ttl) {
+  private static void validateTtl(Duration ttl) {
     if (ttl == null || ttl.isNegative() || ttl.isZero()) {
       Long ttlInMinutes = ttl != null ? ttl.toMinutes() : null;
       String message = String.format(
           "Invalid pegouts signed cache TTL value in minutes supplied: %d", ttlInMinutes);
-      logger.error("[assertValidTtl] {}", message);
+      logger.error("[validateTtl] {}", message);
 
       throw new IllegalArgumentException(message);
     }
