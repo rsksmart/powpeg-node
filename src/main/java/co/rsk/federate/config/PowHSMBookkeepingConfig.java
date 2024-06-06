684995aa8f471af75cf2e915eb715e3ee7bc50d1
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2024-06-06 10:51:34 +0000

    refactor(config): refactor PowHsmBookkeping class to be more consistent with the codebase

--- src/main/java/co/rsk/federate/config/PowHSMBookkeepingConfig.java
+++ src/main/java/co/rsk/federate/config/PowHSMBookkeepingConfig.java
@@ -1,87 +1,86 @@
 package co.rsk.federate.config;
 
-/**
- * Represents the configuration for a signer.
- * Mainly has an identifier for the signer, the
- * type of signer and additional configuration options.
- *
- * @author Pamela Gonzalez
- */
-
 import co.rsk.bitcoinj.core.NetworkParameters;
+import java.math.BigInteger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.math.BigInteger;
 
 public class PowHSMBookkeepingConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(PowHSMBookkeepingConfig.class);
-    public static final BigInteger DIFFICULTY_CAP_MAINNET = new BigInteger("7000000000000000000000");
-    public static final BigInteger DIFFICULTY_CAP_TESTNET = BigInteger.valueOf(1000000000000000L);
-    public static final BigInteger DIFFICULTY_CAP_REGTEST = BigInteger.valueOf(20);
-    private final BigInteger difficultyCap;
-    private BigInteger difficultyTarget = BigInteger.valueOf(3);
-    private int maxAmountBlockHeaders = 7;
-    private long informerInterval = 2_000;
-    private boolean stopBookkeepingScheduler = false;
-    private int maxChunkSizeToHsm = 10;
+
+    enum NetworkDifficultyCap {
+        MAINNET(new BigInteger("7000000000000000000000")),
+        TESTNET(BigInteger.valueOf(1000000000000000L)),
+        REGTEST(BigInteger.valueOf(20L));
+
+        private final BigInteger difficultyCap;
+
+        NetworkDifficultyCap(BigInteger difficultyCap) {
+            this.difficultyCap = difficultyCap;
+        }
+
+        BigInteger getDifficultyCap() {
+            return difficultyCap;
+        }
+    }
+
+    private static final BigInteger DEFAULT_DIFFICULTY_TARGET = BigInteger.valueOf(3);
+    private static final boolean DEFAULT_STOP_BOOKKEPING_SCHEDULER = false;
+    private static final int DEFAULT_MAX_AMOUNT_BLOCK_HEADERS = 7;
+    private static final int DEFAULT_MAX_CHUNK_SIZE = 10;
+    private static final long DEFAULT_INFORMER_INTERVAL = 2_000;
+
+    private final SignerConfig signerConfig;
+    private final String networkParameter;
 
     public PowHSMBookkeepingConfig(SignerConfig signerConfig, String networkParameter) {
-        if (signerConfig.getConfig().hasPath("bookkeeping.difficultyTarget")) {
-            this.difficultyTarget = new BigInteger(signerConfig.getConfig().getString("bookkeeping.difficultyTarget"));
-        }
-        if (signerConfig.getConfig().hasPath("bookkeeping.maxAmountBlockHeaders")) {
-            this.maxAmountBlockHeaders = signerConfig.getConfig().getInt("bookkeeping.maxAmountBlockHeaders");
-        }
-        if (signerConfig.getConfig().hasPath("bookkeeping.informerInterval")) {
-            this.informerInterval = signerConfig.getConfig().getLong("bookkeeping.informerInterval");
-        }
-        if (signerConfig.getConfig().hasPath("bookkeeping.stopBookkeepingScheduler")) {
-            this.stopBookkeepingScheduler = signerConfig.getConfig().getBoolean("bookkeeping.stopBookkeepingScheduler");
-        }
-        if (signerConfig.getConfig().hasPath("bookkeeping.maxChunkSizeToHsm")) {
-            this.maxChunkSizeToHsm = signerConfig.getConfig().getInt("bookkeeping.maxChunkSizeToHsm");
-        }
+        this.signerConfig = signerConfig;
+        this.networkParameter = networkParameter;
+    }
 
+    public BigInteger getDifficultyTarget() {
+        return signerConfig.getConfig().hasPath("bookkeeping.difficultyTarget")
+            ? new BigInteger(signerConfig.getConfig().getString("bookkeeping.difficultyTarget"))
+            : DEFAULT_DIFFICULTY_TARGET;
+    }
+
+    public int getMaxAmountBlockHeaders() {
+        return signerConfig.getConfig().hasPath("bookkeeping.maxAmountBlockHeaders")
+            ? signerConfig.getConfig().getInt("bookkeeping.maxAmountBlockHeaders")
+            : DEFAULT_MAX_AMOUNT_BLOCK_HEADERS;
+    }
+
+    public long getInformerInterval() {
+        return signerConfig.getConfig().hasPath("bookkeeping.informerInterval")
+            ? signerConfig.getConfig().getLong("bookkeeping.informerInterval")
+            : DEFAULT_INFORMER_INTERVAL;
+    }
+
+    public boolean isStopBookkeepingScheduler() {
+        return signerConfig.getConfig().hasPath("bookkeeping.stopBookkeepingScheduler")
+            ? signerConfig.getConfig().getBoolean("bookkeeping.stopBookkeepingScheduler")
+            : DEFAULT_STOP_BOOKKEPING_SCHEDULER;
+    }
+
+    public int getMaxChunkSizeToHsm() {
+        return signerConfig.getConfig().hasPath("bookkeeping.maxChunkSizeToHsm")
+            ? signerConfig.getConfig().getInt("bookkeeping.maxChunkSizeToHsm")
+            : DEFAULT_MAX_CHUNK_SIZE;
+    }
+
+    public BigInteger getDifficultyCap() {
         switch (networkParameter) {
             case NetworkParameters.ID_MAINNET:
-                this.difficultyCap = DIFFICULTY_CAP_MAINNET;
-                break;
+                return NetworkDifficultyCap.MAINNET.getDifficultyCap();
             case NetworkParameters.ID_TESTNET:
-                this.difficultyCap = DIFFICULTY_CAP_TESTNET;
-                break;
+                return NetworkDifficultyCap.TESTNET.getDifficultyCap();
             case NetworkParameters.ID_REGTEST:
-                this.difficultyCap = DIFFICULTY_CAP_REGTEST;
-                break;
+                return NetworkDifficultyCap.REGTEST.getDifficultyCap();
             default:
                 String message = "Invalid network specified for the Bookkeeping Config: " + networkParameter;
                 logger.error(message);
                 throw new IllegalArgumentException(message);
         }
-    }
-
-    public BigInteger getDifficultyTarget() {
-        return difficultyTarget;
-    }
-
-    public int getMaxAmountBlockHeaders() {
-        return maxAmountBlockHeaders;
-    }
-
-    public long getInformerInterval() {
-        return informerInterval;
-    }
-
-    public boolean isStopBookkeepingScheduler() {
-        return stopBookkeepingScheduler;
-    }
-
-    public int getMaxChunkSizeToHsm() {
-        return maxChunkSizeToHsm;
-    }
-
-    public BigInteger getDifficultyCap() {
-        return difficultyCap;
     }
 }
