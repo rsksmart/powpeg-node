c7396093de1022e5c68dc5e60d86f3469819940e
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2024-06-24 08:41:05 +0000

    refactor(config): default config values should be empty strings

--- src/main/java/co/rsk/federate/signing/hsm/config/PowHSMConfigParameter.java
+++ src/main/java/co/rsk/federate/signing/hsm/config/PowHSMConfigParameter.java
@@ -5,13 +5,13 @@ import java.util.function.Function;
 
 public enum PowHSMConfigParameter {
   // general
-  HOST("host", null),
-  PORT("port", null),
+  HOST("host", ""),
+  PORT("port", ""),
   INTERVAL_BETWEEN_ATTEMPTS("intervalBetweenAttempts", "1_000"),
   MAX_ATTEMPTS("maxAttempts", "2"),
   SOCKET_TIMEOUT("socketTimeout", "20_000"),
   // bookkeeping
-  DIFFICULTY_TARGET("bookkeeping.difficultyTarget", null),
+  DIFFICULTY_TARGET("bookkeeping.difficultyTarget", ""),
   INFORMER_INTERVAL("bookkeeping.informerInterval", "360_000"), // 6 minutes in milliseconds
   MAX_AMOUNT_BLOCK_HEADERS("bookkeeping.maxAmountBlockHeaders", "100"),
   MAX_CHUNK_SIZE_TO_HSM("bookkeeping.maxChunkSizeToHsm", "100"),
@@ -32,8 +32,8 @@ public enum PowHSMConfigParameter {
   public <T> T getDefaultValue(Function<String, T> parser) {
     Objects.requireNonNull(parser);
 
-    if (defaultValue == null) {
-      return null;
+    if (defaultValue.isEmpty()) {
+      throw new IllegalStateException("No default value present");
     }
     return parser.apply(defaultValue);
   }
