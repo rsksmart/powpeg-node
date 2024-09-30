96b686131355e3fcd077249837774ed0836284c8
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2024-09-30 10:41:04 +0000

    refactor(fed): federation watcher tests

--- src/main/java/co/rsk/federate/FederationWatcher.java
+++ src/main/java/co/rsk/federate/FederationWatcher.java
@@ -48,7 +48,7 @@ public class FederationWatcher {
         listeners.add(listener);
     }
 
-    private class FederationWatcherRskListener extends EthereumListenerAdapter {
+    public class FederationWatcherRskListener extends EthereumListenerAdapter {
         @Override
         public void onBestBlock(org.ethereum.core.Block block, List<TransactionReceipt> receipts) {
             // Updating state only when the best block changes still "works",
