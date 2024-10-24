84006ed026177d92b8b21002b2e3a7ac0e7ffc74
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2024-10-24 14:21:37 +0000

    feat(watcher): change stop and starting logic when active and retiring federation

--- src/main/java/co/rsk/federate/watcher/FederationWatcherListenerImpl.java
+++ src/main/java/co/rsk/federate/watcher/FederationWatcherListenerImpl.java
@@ -3,27 +3,22 @@ package co.rsk.federate.watcher;
 import co.rsk.federate.BtcToRskClient;
 import co.rsk.federate.btcreleaseclient.BtcReleaseClient;
 import co.rsk.peg.federation.Federation;
-import co.rsk.peg.federation.FederationMember;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.Objects;
-import java.util.Optional;
 
 public class FederationWatcherListenerImpl implements FederationWatcherListener {
 
     private static final Logger logger = LoggerFactory.getLogger(FederationWatcherListenerImpl.class);
     
-    private final FederationMember federationMember;
     private final BtcToRskClient btcToRskClientActive;
     private final BtcToRskClient btcToRskClientRetiring;
     private final BtcReleaseClient btcReleaseClient;
 
     public FederationWatcherListenerImpl(
-            FederationMember federationMember,
             BtcToRskClient btcToRskClientActive,
             BtcToRskClient btcToRskClientRetiring,
             BtcReleaseClient btcReleaseClient) {
-        this.federationMember = Objects.requireNonNull(federationMember);
         this.btcToRskClientActive = btcToRskClientActive;
         this.btcToRskClientRetiring = btcToRskClientRetiring;
         this.btcReleaseClient = btcReleaseClient;
@@ -36,31 +31,37 @@ public class FederationWatcherListenerImpl implements FederationWatcherListener 
 
     @Override
     public void onRetiringFederationChange(Federation newFederation) {
-        triggerClientChange(btcToRskClientRetiring, newFederation);
+        if (newFederation == null) {
+            triggerClearingRetiringFederationClient();
+        } else {
+            triggerClientChange(btcToRskClientRetiring, newFederation);
+        }
     }
 
     private void triggerClientChange(BtcToRskClient btcToRskClient, Federation newFederation) {
-        // Stop the current clients
-        btcToRskClient.stop();
-        Optional.ofNullable(newFederation).ifPresent(btcReleaseClient::stop);
+        // This method assumes that the new federation cannot be null
+        Objects.requireNonNull(newFederation);
+      
+        try {
+            // Stop the current clients
+            btcToRskClient.stop();
+            btcReleaseClient.stop(newFederation);
 
-        // Check if this federator is part of the new federation
-        if (newFederation == null || !newFederation.isMember(federationMember)) {
-            logger.warn(
-                "[triggerClientChange] This federator node ({}) is not part of the new federation ({}). Check your configuration for signers BTC, RSK, and MST keys",
-                federationMember,
-                Optional.ofNullable(newFederation).map(Federation::getAddress).orElse(null));
-            return;
+            // Start the current clients
+            btcToRskClient.start(newFederation);
+            btcReleaseClient.start(newFederation);
+
+            logger.info(
+                "[triggerClientChange] Joined {} federation", newFederation.getAddress());
+        } catch (Exception e) {
+            logger.error(
+                "[triggerClientChange] This federation ({}) cannot be started: {}",
+                newFederation.getAddress(),
+                e.getMessage());
         }
-
-        // Start clients if federator is part of the new federation
-        String federationAddress = newFederation.getAddress().toString();
-        logger.debug(
-            "[triggerClientChange] Starting lock and release clients since I belong to federation {}", federationAddress);
-        logger.info(
-            "[triggerClientChange] Joined to {} federation", federationAddress);
-
-        btcToRskClient.start(newFederation);
-        btcReleaseClient.start(newFederation);
+    }
+    
+    private void triggerClearingRetiringFederationClient() {
+        btcToRskClientRetiring.stop();
     }
 }
