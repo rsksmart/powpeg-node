3d1e1df742e886741561270041570d01cd43a042
nathanieliov <nathaniel.calderon@iovlabs.org>
2025-02-19 00:35:26 -0400

    Resolve Sonar issues

--- src/test/java/co/rsk/federate/signing/hsm/requirements/ReleaseRequirementsEnforcerTest.java
+++ src/test/java/co/rsk/federate/signing/hsm/requirements/ReleaseRequirementsEnforcerTest.java
@@ -17,32 +17,28 @@ import org.junit.jupiter.api.Test;
 class ReleaseRequirementsEnforcerTest {
 
     private AncestorBlockUpdater ancestorBlockUpdater;
-    private ReleaseRequirementsEnforcer enforcer;
+    private ReleaseRequirementsEnforcer releaseRequirementsEnforcer;
 
     @BeforeEach
     void setup() {
         ancestorBlockUpdater = mock(AncestorBlockUpdater.class);
-        enforcer = new ReleaseRequirementsEnforcer(ancestorBlockUpdater);
+        releaseRequirementsEnforcer = new ReleaseRequirementsEnforcer(ancestorBlockUpdater);
     }
 
     @Test
     void enforce_does_nothing_if_version_one() throws Exception {
-        AncestorBlockUpdater ancestorBlockUpdater = mock(AncestorBlockUpdater.class);
-        ReleaseRequirementsEnforcer enforcer = new ReleaseRequirementsEnforcer(ancestorBlockUpdater);
-
-        enforcer.enforce(1, mock(ReleaseCreationInformation.class));
-
+        releaseRequirementsEnforcer.enforce(1, mock(ReleaseCreationInformation.class));
         verify(ancestorBlockUpdater, never()).ensureAncestorBlockInPosition(any());
     }
 
     @Test
     void enforce_version_two_ok() throws Exception {
-        test_enforce_version(ancestorBlockUpdater, enforcer, HSMVersion.V2);
+        test_enforce_version(ancestorBlockUpdater, releaseRequirementsEnforcer, HSMVersion.V2);
     }
 
     @Test
     void enforce_version_four_ok() throws Exception {
-        test_enforce_version(ancestorBlockUpdater, enforcer, HSMVersion.V4);
+        test_enforce_version(ancestorBlockUpdater, releaseRequirementsEnforcer, HSMVersion.V4);
     }
 
     void test_enforce_version(AncestorBlockUpdater ancestorBlockUpdater, ReleaseRequirementsEnforcer enforcer, HSMVersion version) throws Exception {
@@ -52,25 +48,23 @@ class ReleaseRequirementsEnforcerTest {
     }
 
     @Test
-    void enforce_version_three_fail() throws Exception {
+    void enforce_version_three_fail() {
         Assertions.assertThrows(ReleaseRequirementsEnforcerException.class, () -> {
-            enforcer.enforce(3, mock(ReleaseCreationInformation.class));
+            releaseRequirementsEnforcer.enforce(3, mock(ReleaseCreationInformation.class));
         }, "Unsupported version 3");
     }
 
     @Test
     void enforce_version_two_updater_fails() throws Exception {
-        AncestorBlockUpdater ancestorBlockUpdater = mock(AncestorBlockUpdater.class);
-        doThrow(new Exception()).when(ancestorBlockUpdater).ensureAncestorBlockInPosition(any());
-        ReleaseRequirementsEnforcer enforcer = new ReleaseRequirementsEnforcer(ancestorBlockUpdater);
+        AncestorBlockUpdater blockUpdater = mock(AncestorBlockUpdater.class);
+        doThrow(new Exception()).when(blockUpdater).ensureAncestorBlockInPosition(any());
+        ReleaseRequirementsEnforcer enforcer = new ReleaseRequirementsEnforcer(blockUpdater);
 
         assertThrows(ReleaseRequirementsEnforcerException.class, () -> enforcer.enforce(HSMVersion.V2.getNumber(), mock(ReleaseCreationInformation.class)));
     }
 
     @Test
     void enforce_invalid_version() {
-        ReleaseRequirementsEnforcer enforcer = new ReleaseRequirementsEnforcer(mock(AncestorBlockUpdater.class));
-
-        assertThrows(ReleaseRequirementsEnforcerException.class, () -> enforcer.enforce(-5, mock(ReleaseCreationInformation.class)));
+        assertThrows(ReleaseRequirementsEnforcerException.class, () -> releaseRequirementsEnforcer.enforce(-5, mock(ReleaseCreationInformation.class)));
     }
 }
