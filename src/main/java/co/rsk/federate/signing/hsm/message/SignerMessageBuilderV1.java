31f8f353e2ca2724fbed7f3367a62f47a109f99b
julia-zack <julia.zack@iovlabs.org>
2025-06-12 15:09:37 -0300

    Send just needed parameters to SignerMessageBuilder constructor

--- src/main/java/co/rsk/federate/signing/hsm/message/SignerMessageBuilderV1.java
+++ src/main/java/co/rsk/federate/signing/hsm/message/SignerMessageBuilderV1.java
@@ -1,11 +1,12 @@
 package co.rsk.federate.signing.hsm.message;
 
+import co.rsk.bitcoinj.core.BtcTransaction;
 import co.rsk.bitcoinj.core.Sha256Hash;
 
 public class SignerMessageBuilderV1 extends SignerMessageBuilder {
 
-    public SignerMessageBuilderV1(ReleaseCreationInformation releaseCreationInformation) {
-        super(releaseCreationInformation);
+    public SignerMessageBuilderV1(BtcTransaction unsignedBtcTx) {
+        super(unsignedBtcTx);
     }
 
     public SignerMessage buildMessageForIndex(int inputIndex) {
