c8eb05dd9675a15052e752867345428fa20c398c
julia-zack <julia.zack@iovlabs.org>
2025-06-13 13:46:22 -0300

    Create new SigHashCalculator interface and legacy and segwit impls, and use them for building the SignerMessageBuilder

--- src/main/java/co/rsk/federate/signing/hsm/message/SignerMessageBuilder.java
+++ src/main/java/co/rsk/federate/signing/hsm/message/SignerMessageBuilder.java
@@ -1,46 +1,17 @@
 package co.rsk.federate.signing.hsm.message;
 
 import co.rsk.bitcoinj.core.*;
-import co.rsk.bitcoinj.script.Script;
-
-import java.util.List;
-
-import static co.rsk.peg.bitcoin.BitcoinUtils.extractRedeemScriptFromInput;
-import static co.rsk.peg.bitcoin.BitcoinUtils.inputHasWitness;
+import co.rsk.federate.signing.SigHashCalculator;
 
 public abstract class SignerMessageBuilder {
-    protected List<Coin> releaseOutpointsValues;
+
     protected BtcTransaction unsignedBtcTx;
+    protected SigHashCalculator sigHashCalculator;
 
-    protected SignerMessageBuilder(BtcTransaction unsignedBtcTx) {
+    protected SignerMessageBuilder(BtcTransaction unsignedBtcTx, SigHashCalculator sigHashCalculator) {
         this.unsignedBtcTx = unsignedBtcTx;
-    }
-
-    protected SignerMessageBuilder(BtcTransaction unsignedBtcTx, List<Coin> releaseOutpointsValues) {
-        this.unsignedBtcTx = unsignedBtcTx;
-        this.releaseOutpointsValues = releaseOutpointsValues;
+        this.sigHashCalculator = sigHashCalculator;
     }
 
     public abstract SignerMessage buildMessageForIndex(int inputIndex) throws SignerMessageBuilderException;
-
-    protected Sha256Hash getSigHashForInputIndex(int inputIndex) {
-        if (inputHasWitness(unsignedBtcTx, inputIndex)) {
-            return extractRedeemScriptFromInput(unsignedBtcTx, inputIndex)
-                .map(redeemScript -> getSegwitSigHashForInputIndex(inputIndex, redeemScript))
-                .orElseThrow(() -> new IllegalStateException("Couldn't extract redeem script from input"));
-        }
-
-        return extractRedeemScriptFromInput(unsignedBtcTx, inputIndex)
-            .map(redeemScript -> getLegacySigHashForInputIndex(inputIndex, redeemScript))
-            .orElseThrow(() -> new IllegalStateException("Couldn't extract redeem script from input"));
-    }
-
-    private Sha256Hash getLegacySigHashForInputIndex(int inputIndex, Script redeemScript) {
-        return unsignedBtcTx.hashForSignature(inputIndex, redeemScript, BtcTransaction.SigHash.ALL, false);
-    }
-
-    private Sha256Hash getSegwitSigHashForInputIndex(int inputIndex, Script redeemScript) {
-        Coin prevValue = releaseOutpointsValues.get(inputIndex);
-        return unsignedBtcTx.hashForWitnessSignature(inputIndex, redeemScript, prevValue, BtcTransaction.SigHash.ALL, false);
-    }
 }
--- src/main/java/co/rsk/federate/signing/hsm/message/SignerMessageBuilderV1.java
+++ src/main/java/co/rsk/federate/signing/hsm/message/SignerMessageBuilderV1.java
@@ -2,15 +2,16 @@ package co.rsk.federate.signing.hsm.message;
 
 import co.rsk.bitcoinj.core.BtcTransaction;
 import co.rsk.bitcoinj.core.Sha256Hash;
+import co.rsk.federate.signing.SigHashCalculator;
 
 public class SignerMessageBuilderV1 extends SignerMessageBuilder {
 
-    public SignerMessageBuilderV1(BtcTransaction unsignedBtcTx) {
-        super(unsignedBtcTx);
+    public SignerMessageBuilderV1(BtcTransaction unsignedBtcTx, SigHashCalculator sigHashCalculator) {
+        super(unsignedBtcTx, sigHashCalculator);
     }
 
     public SignerMessage buildMessageForIndex(int inputIndex) {
-        Sha256Hash sigHash = getSigHashForInputIndex(inputIndex);
+        Sha256Hash sigHash = sigHashCalculator.calculate(unsignedBtcTx, inputIndex);
         return new SignerMessageV1(sigHash.getBytes());
     }
 }
