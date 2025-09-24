b1f30714f7cb04a1ae524751d4419d92a9a3c336
julia-zack <julia.zack@iovlabs.org>
2025-09-24 19:39:23 -0300

    Add new test for real testnet pegin when two feds are being listened

--- src/test/java/co/rsk/federate/bitcoin/BitcoinWrapperImplTest.java
+++ src/test/java/co/rsk/federate/bitcoin/BitcoinWrapperImplTest.java
@@ -54,6 +54,8 @@ class BitcoinWrapperImplTest {
     private BtcToRskClientFileStorage btcToRskClientFileStorage;
     private FederatorSupport federatorSupport;
     private BitcoinWrapperImpl bitcoinWrapper;
+    private BtcToRskClientBuilder btcToRskClientBuilder;
+    private TransactionListener listener;
 
     @TempDir
     private Path tempDir;
@@ -81,7 +83,7 @@ class BitcoinWrapperImplTest {
         btcToRskClientFileStorage = new BtcToRskClientFileStorageImpl(fileStorageInfo);
     }
 
-    private void setUpListenerAndWrapperWithFederation(Federation federationToListen) throws Exception {
+    private void setUpListenerAndBitcoinWrapper() throws Exception {
         BtcLockSenderProvider btcLockSenderProvider = new BtcLockSenderProvider();
         PeginInstructionsProvider peginInstructionsProvider = new PeginInstructionsProvider();
 
@@ -99,27 +101,33 @@ class BitcoinWrapperImplTest {
         bitcoinWrapper.setup(peerAddresses);
         bitcoinWrapper.start();
 
-        BtcToRskClientBuilder btcToRskClientBuilder = BtcToRskClientBuilder.builder();
-        TransactionListener listener = btcToRskClientBuilder
+        btcToRskClientBuilder = BtcToRskClientBuilder.builder();
+        listener = btcToRskClientBuilder
             .withBitcoinWrapper(bitcoinWrapper)
             .withFederatorSupport(federatorSupport)
             .withBridgeConstants(bridgeConstants)
             .withBtcToRskClientFileStorage(btcToRskClientFileStorage)
             .withBtcLockSenderProvider(btcLockSenderProvider)
             .withPeginInstructionsProvider(peginInstructionsProvider)
-            .withFederation(federationToListen)
             .build();
+    }
 
-        bitcoinWrapper.addFederationListener(federationToListen, listener);
+    private void listenToFederation(Federation federationToListenTo) throws Exception {
+        listener = btcToRskClientBuilder
+            .withFederation(federationToListenTo)
+            .build();
+        bitcoinWrapper.addFederationListener(federationToListenTo, listener);
     }
 
     @Nested
     @TestInstance(TestInstance.Lifecycle.PER_CLASS)
     @Tag("Pegins in testnet below minimum that should not be added to tx proofs list")
     class TestnetPeginsBelowMinimumTests {
-        // tests for https://mempool.space/testnet/tx/30adc33282d1737085033cc63f77ed484b10eef79e0a3cbed00fd4f8ad6fb134
-        // and https://mempool.space/testnet/tx/d1ad68905267970043fc282fb16c9aed018f14eca4a433099ccf71279e2666d1
-        Federation federation;
+        // tests for https://mempool.space/testnet/tx/30adc33282d1737085033cc63f77ed484b10eef79e0a3cbed00fd4f8ad6fb134,
+        // https://mempool.space/testnet/tx/d1ad68905267970043fc282fb16c9aed018f14eca4a433099ccf71279e2666d1
+        // and https://mempool.space/testnet/tx/529e34c983aa4358ad94d5887ba0d89019bdfb5d97d0392f4da000cefc3e1cc2
+        Federation p2shErpFederation;
+        Federation p2shP2wshErpFederation;
 
         @BeforeEach
         void testnetSetUp() {
@@ -127,8 +135,9 @@ class BitcoinWrapperImplTest {
             FederationConstants federationConstants = bridgeConstants.getFederationConstants();
             thinNetworkParameters = bridgeConstants.getBtcParams();
             originalNetworkParameters = ThinConverter.toOriginalInstance(bridgeConstants.getBtcParamsString());
+            btcContext = new Context(originalNetworkParameters);
 
-            List<BtcECKey> btcPubKeys = Arrays.asList(
+            List<BtcECKey> p2shErpFedBtcPubKeys = Arrays.asList(
                 BtcECKey.fromPublicOnly(Hex.decode("02099fd69cf6a350679a05593c3ff814bfaa281eb6dde505c953cf2875979b1209")),
                 BtcECKey.fromPublicOnly(Hex.decode("0222caa9b1436ebf8cdf0c97233a8ca6713ed37b5105bcbbc674fd91353f43d9f7")),
                 BtcECKey.fromPublicOnly(Hex.decode("022a159227df514c7b7808ee182ae07d71770b67eda1e5ee668272761eefb2c24c")),
@@ -140,26 +149,66 @@ class BitcoinWrapperImplTest {
                 BtcECKey.fromPublicOnly(Hex.decode("03eae17ad1d0094a5bf33c037e722eaf3056d96851450fb7f514a9ed3af1dbb570"))
             );
 
-            List<org.ethereum.crypto.ECKey> rskPubKeys = btcPubKeys.stream()
+            List<org.ethereum.crypto.ECKey> p2shErpFedRskPubKeys = p2shErpFedBtcPubKeys.stream()
                 .map(BtcECKey::getPubKey)
                 .map(org.ethereum.crypto.ECKey::fromPublicOnly)
                 .toList();
 
-            List<FederationMember> members = IntStream.range(0, btcPubKeys.size())
+            List<FederationMember> p2shErpFedMembers = IntStream.range(0, p2shErpFedBtcPubKeys.size())
                 .mapToObj(i -> new FederationMember(
-                    btcPubKeys.get(i),
-                    rskPubKeys.get(i),
-                    rskPubKeys.get(i)))
+                    p2shErpFedBtcPubKeys.get(i),
+                    p2shErpFedRskPubKeys.get(i),
+                    p2shErpFedRskPubKeys.get(i)))
                 .toList();
 
-            FederationArgs federationArgs = new FederationArgs(
-                members,
+            FederationArgs p2shErpFederationArgs = new FederationArgs(
+                p2shErpFedMembers,
                 Instant.ofEpochSecond(1L),
                 1L,
                 bridgeConstants.getBtcParams()
             );
-            federation = FederationFactory.buildP2shErpFederation(
-                federationArgs,
+            p2shErpFederation = FederationFactory.buildP2shErpFederation(
+                p2shErpFederationArgs,
+                federationConstants.getErpFedPubKeysList(),
+                federationConstants.getErpFedActivationDelay()
+            );
+
+            List<BtcECKey> p2shP2wshErpFedBtcPubKeys = Arrays.asList(
+                BtcECKey.fromPublicOnly(Hex.decode("02099fd69cf6a350679a05593c3ff814bfaa281eb6dde505c953cf2875979b1209")),
+                BtcECKey.fromPublicOnly(Hex.decode("0222caa9b1436ebf8cdf0c97233a8ca6713ed37b5105bcbbc674fd91353f43d9f7")),
+                BtcECKey.fromPublicOnly(Hex.decode("022a159227df514c7b7808ee182ae07d71770b67eda1e5ee668272761eefb2c24c")),
+                BtcECKey.fromPublicOnly(Hex.decode("02afc230c2d355b1a577682b07bc2646041b5d0177af0f98395a46018da699b6da")),
+                BtcECKey.fromPublicOnly(Hex.decode("02b1645d3f0cff938e3b3382b93d2d5c082880b86cbb70b6600f5276f235c28392")),
+                BtcECKey.fromPublicOnly(Hex.decode("039ee63f1e22ed0eb772fe0a03f6c34820ce8542f10e148bc3315078996cb81b25")),
+                BtcECKey.fromPublicOnly(Hex.decode("03d25ed2fcf9e05537f6e1daa7affcafdb3effc9f68cb2aecdcad66c901ae1b657")),
+                BtcECKey.fromPublicOnly(Hex.decode("03e2fbfd55959660c94169320ed0a778507f8e4c7a248a71c6599a4ce8a3d956ac")),
+                BtcECKey.fromPublicOnly(Hex.decode("03eae17ad1d0094a5bf33c037e722eaf3056d96851450fb7f514a9ed3af1dbb570")),
+                BtcECKey.fromPublicOnly(Hex.decode("0292039a22480290feccf059d7be7cb541cba6af97897526befbf3e411dfa9ef8d")),
+                BtcECKey.fromPublicOnly(Hex.decode("02bcb15b9e59701700e24a53ddbe3f828e272a0a4b5d4385155d0b1c89019f8b14")),
+                BtcECKey.fromPublicOnly(Hex.decode("03bcfe265c88c1cd7cd7d16deac7d91e45dbf82599e8199fda0a909ec15260ed59")),
+                BtcECKey.fromPublicOnly(Hex.decode("03cac7e78da7ca46910b3326438a9e784e8b1af5a3253fe7bd6cfac91444939015"))
+            );
+
+            List<org.ethereum.crypto.ECKey> p2shP2wshErpFedRskPubKeys = p2shErpFedBtcPubKeys.stream()
+                .map(BtcECKey::getPubKey)
+                .map(org.ethereum.crypto.ECKey::fromPublicOnly)
+                .toList();
+
+            List<FederationMember> p2shP2wshErpFedMembers = IntStream.range(0, p2shErpFedBtcPubKeys.size())
+                .mapToObj(i -> new FederationMember(
+                    p2shP2wshErpFedBtcPubKeys.get(i),
+                    p2shP2wshErpFedRskPubKeys.get(i),
+                    p2shP2wshErpFedRskPubKeys.get(i)))
+                .toList();
+
+            FederationArgs p2shP2wshErpFederationArgs = new FederationArgs(
+                p2shP2wshErpFedMembers,
+                Instant.ofEpochSecond(1L),
+                1L,
+                bridgeConstants.getBtcParams()
+            );
+            p2shP2wshErpFederation = FederationFactory.buildP2shP2wshErpFederation(
+                p2shP2wshErpFederationArgs,
                 federationConstants.getErpFedPubKeysList(),
                 federationConstants.getErpFedActivationDelay()
             );
@@ -168,8 +217,8 @@ class BitcoinWrapperImplTest {
         @Test
         void coinsReceivedOrSent_legacyPegin_amountBelowMinimum_shouldNotListenTx() throws Exception {
             // Arrange
-            btcContext = new Context(originalNetworkParameters);
-            setUpListenerAndWrapperWithFederation(federation);
+            setUpListenerAndBitcoinWrapper();
+            listenToFederation(p2shErpFederation);
 
             byte[] rawTx = Hex.decode("020000000001010b93ce79620bd58a84855aa3a452e450e4896317267ae036a4db226f97ced0190000000000fdffffff02e80300000000000017a91423b8cdb52fd91d35d6ec5821ef91c9d6da67b78a871c0c0000000000001600149d15881009505f03faa87801db5a66dcd113b97b024730440220464615a947d95ba1306193c7de126390d256b770eae2f3995d480c96d25ba30402204f56d759826d742ad10f130096e35dce3caf9aef63cbc78099ab96052ce8486f01210357ca84c0361f7669df3e1654620f8d971288ac8915685b0bb49f153bef4f51499fdd4500");
             BtcTransaction peginThinInstance = new BtcTransaction(thinNetworkParameters, rawTx);
@@ -185,7 +234,8 @@ class BitcoinWrapperImplTest {
         @Test
         void coinsReceivedOrSent_peginV1_amountBelowMinimum_shouldNotListenTx() throws Exception {
             // Arrange
-            setUpListenerAndWrapperWithFederation(federation);
+            setUpListenerAndBitcoinWrapper();
+            listenToFederation(p2shErpFederation);
 
             byte[] rawTx = Hex.decode("020000000001012a770aebe30ce215a949b1a0e70a64993df73c7adb62f2e43b66054bdbd675080200000000ffffffff030000000000000000306a2e52534b54017509517a1880b14c9d734c55fac18c7737ec11c5011ae302de6607907116810e598b83897b00f764d5801a06000000000017a91423b8cdb52fd91d35d6ec5821ef91c9d6da67b78a87f8070100000000001600149b6d476d887db413ed0a59fbb1ea80ed41641e7002483045022100be6dbbf87227fd75f3b67e2f8a83c52965a42e89db16a91d8e4fd38d98afab4102207c1aff9e7777a923e67c9998acfbe6bf3f362adecddcdf823cdad3cd9d4462b601210296b60d2b92e4ba3f1948e00412d5fdc4ec0586830660c806ffe2214daa25fce900000000");
             BtcTransaction peginThinInstance = new BtcTransaction(thinNetworkParameters, rawTx);
@@ -205,13 +255,32 @@ class BitcoinWrapperImplTest {
             Set<Sha256Hash> txProofsKeySet = transactionProofs.keySet();
             assertEquals(0, txProofsKeySet.size());
         }
+
+        @Test
+        void coinsReceivedOrSent_peginV1_amountBelowMinimum_listeningToRetiringAndActiveFed_listensTx() throws Exception {
+            // Arrange
+            setUpListenerAndBitcoinWrapper();
+            listenToFederation(p2shErpFederation);
+            listenToFederation(p2shP2wshErpFederation);
+
+            byte[] rawTx = Hex.decode("02000000000101bbe9a9aa3a5c16e9628793eedb53972219397744de49a865190b0a14ef873e520200000000fdffffff0300000000000000001b6a1952534b540162db6c4b118d7259c23692b162829e6bd5e4d5b0a08601000000000017a914a144f1e7626f6b8d7ce7986d2c15ef115394dea9875be64c0500000000160014ab44f1b899822ed19e488b20ce0c0fafb0dd40980248304502210086cdb085b197feb254dd91d74579a068b92818de379668023f19df65948e191b02205c69357c23ae7e2881613a711fe72eb74d1b0ded193aa7db2485f6c90b7d1d5f0121029a3a61efa700dc18b06d0f1361564b92547d1fed2499cd871a31655075789eb555de4700");
+            BtcTransaction peginThinInstance = new BtcTransaction(thinNetworkParameters, rawTx);
+            Transaction pegin = ThinConverter.toOriginalInstance(thinNetworkParameters.getId(), peginThinInstance);
+
+            // Act
+            bitcoinWrapper.coinsReceivedOrSent(pegin);
+
+            // assert
+            assertWTxIdWasAddedToProofs(pegin);
+        }
     }
 
     @ParameterizedTest
     @MethodSource("fedArgs")
     void coinsReceivedOrSent_validLegacyPegIn_shouldListenTx(Federation federation) throws Exception {
         // Arrange
-        setUpListenerAndWrapperWithFederation(federation);
+        setUpListenerAndBitcoinWrapper();
+        listenToFederation(federation);
         Transaction pegin = createLegacyPegIn(federation);
 
         // Act
@@ -225,7 +294,8 @@ class BitcoinWrapperImplTest {
     @MethodSource("fedArgs")
     void coinsReceivedOrSent_validPeginV1_shouldListenTx(Federation federation) throws Exception {
         // Arrange
-        setUpListenerAndWrapperWithFederation(federation);
+        setUpListenerAndBitcoinWrapper();
+        listenToFederation(federation);
         Transaction pegin = createValidPegInV1(federation.getAddress());
 
         // Act
@@ -273,8 +343,9 @@ class BitcoinWrapperImplTest {
     @MethodSource("fedArgs")
     void coinsReceivedOrSent_validPegOutTx_shouldListenTx(Federation federation) throws Exception {
         // Arrange
+        setUpListenerAndBitcoinWrapper();
+        listenToFederation(federation);
         final Transaction pegout = createPegOutTx(federation);
-        setUpListenerAndWrapperWithFederation(federation);
 
         // Act
         bitcoinWrapper.coinsReceivedOrSent(pegout);
@@ -344,7 +415,8 @@ class BitcoinWrapperImplTest {
         when(federatorSupport.getFederationAddress()).thenReturn(activeFederation.getAddress());
         when(federatorSupport.getRetiringFederationAddress()).thenReturn(Optional.of(retiringFederation.getAddress()));
 
-        setUpListenerAndWrapperWithFederation(activeFederation);
+        setUpListenerAndBitcoinWrapper();
+        listenToFederation(activeFederation);
         Transaction migrationTx = createMigrationTx(retiringFederation, activeFederation);
 
         // act
