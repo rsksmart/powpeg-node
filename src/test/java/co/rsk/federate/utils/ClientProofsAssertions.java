31ecc7039f7606a84fef422ae9a0c1436db1b3b3
julia-zack <julia.zack@iovlabs.org>
2026-04-15 14:25:14 -0300

    Add assertion methods. Refactor to make checks more accurate

--- src/test/java/co/rsk/federate/utils/ClientProofsAssertions.java
+++ src/test/java/co/rsk/federate/utils/ClientProofsAssertions.java
@@ -1,5 +1,6 @@
 package co.rsk.federate.utils;
 
+import co.rsk.federate.BtcToRskClient;
 import co.rsk.federate.Proof;
 import co.rsk.federate.io.BtcToRskClientFileData;
 import co.rsk.federate.io.BtcToRskClientFileStorage;
@@ -12,20 +13,32 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ClientProofsAssertions {
+
+    public static void assertWTxIdIsInTxsToBeSentMap(BtcToRskClient btcToRskClient, Transaction tx) {
+        assertTrue(btcToRskClient.getTransactionsToSendToRsk().containsKey(tx.getWTxId()));
+    }
+
+    public static void assertWTxIdIsNotInTxsToBeSentMap(BtcToRskClient btcToRskClient, Transaction tx) {
+        assertFalse(btcToRskClient.getTransactionsToSendToRsk().containsKey(tx.getWTxId()));
+    }
 
     public static void assertWTxIdIsInProofsFile(NetworkParameters networkParameters, BtcToRskClientFileStorage btcToRskClientFileStorage, Transaction tx) throws IOException {
         BtcToRskClientFileData fileData = btcToRskClientFileStorage.read(networkParameters).getData();
         Map<Sha256Hash, List<Proof>> transactionProofs = fileData.getTransactionProofs();
 
         Set<Sha256Hash> txProofsKeySet = transactionProofs.keySet();
-        assertEquals(1, txProofsKeySet.size());
+        assertTrue(txProofsKeySet.contains(tx.getWTxId()));
+    }
 
-        Sha256Hash wTxId = tx.getWTxId();
-        Sha256Hash savedWTxId = txProofsKeySet.iterator().next();
-        assertEquals(wTxId, savedWTxId);
+    public static void assertWTxIdIsNotInProofsFile(NetworkParameters networkParameters, BtcToRskClientFileStorage btcToRskClientFileStorage, Transaction tx) throws IOException {
+        BtcToRskClientFileData fileData = btcToRskClientFileStorage.read(networkParameters).getData();
+        Map<Sha256Hash, List<Proof>> transactionProofs = fileData.getTransactionProofs();
+
+        Set<Sha256Hash> txProofsKeySet = transactionProofs.keySet();
+        assertFalse(txProofsKeySet.contains(tx.getWTxId()));
     }
 
     public static void assertProofsFileIsEmpty(NetworkParameters networkParameters, BtcToRskClientFileStorage btcToRskClientFileStorage) throws IOException {
