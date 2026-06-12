e9023aa2b6f61315acfee7e962f4ee33190dc0e1
julia-zack <julia.zack@iovlabs.org>
2026-06-12 18:20:02 -0300

    Remove unused imports in CoinbaseInformationTest

--- src/test/java/co/rsk/federate/CoinbaseInformationTest.java
+++ src/test/java/co/rsk/federate/CoinbaseInformationTest.java
@@ -1,9 +1,7 @@
 package co.rsk.federate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Collections;
 import java.util.List;
