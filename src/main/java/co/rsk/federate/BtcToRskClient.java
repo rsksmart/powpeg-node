9dde3460c630bd29dbabbf17a7dd2a854507cf0c
Marcos <marcos@rootstocklabs.com>
2026-08-13 13:42:35 -0300

    Remove unthrown expection from method signature

--- src/main/java/co/rsk/federate/BtcToRskClient.java
+++ src/main/java/co/rsk/federate/BtcToRskClient.java
@@ -395,7 +395,7 @@ public class BtcToRskClient implements BlockListener, TransactionListener {
         }
     }
 
-    protected int updateBridgeBtcBlockchain() throws BlockStoreException, IOException {
+    protected int updateBridgeBtcBlockchain() throws BlockStoreException {
         int bridgeBtcBlockchainBestChainHeight = federatorSupport.getBtcBlockchainBestChainHeight();
         int federatorBtcBlockchainBestChainHeight = bitcoinWrapper.getBestChainHeight();
         if (federatorBtcBlockchainBestChainHeight > bridgeBtcBlockchainBestChainHeight) {
